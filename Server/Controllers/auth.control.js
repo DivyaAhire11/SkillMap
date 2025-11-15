6aa1c7937446bf8bb36032a6b2362a2e90568bfc
DivyaAhire11 <divyaahire121@gmail.com>
2025-11-15 18:53:22 +0530

    Login SuccessFully

--- Server/Controllers/auth.control.js
+++ Server/Controllers/auth.control.js
@@ -15,7 +15,7 @@ import jwt from "jsonwebtoken";
  LOGIN API
  1 . User input (username , email ,password) POST api
  2 . Validation : Check if the request body contains required fields
- 3 . Authentication : Look up the user in your database by email/username.
+ 3 . Authentication : Look up the user in your database by email.
      Compare the provided password with the stored (hashed) password using a hashing algorithm like bcrypt.
  4 . Token Generate : If authentication succeeds, generate a JWT (JSON Web Token) or session token.
  5 . Responce : success responce 
@@ -23,73 +23,80 @@ import jwt from "jsonwebtoken";
  */
 
 const SignUp = async (req, res) => {
-    let { name, email, password } = req.body;
-    if (!name || !email || !password) {
-        return responder(res, 400, "Must provide name, email, and password");
-    }
+    try {
+        let { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return responder(res, 400, "Must provide name, email, and password");
+        }
 
-    let ISUserExits = await User.findOne({ email: email })
+        let ISUserExits = await User.findOne({ email: email })
 
-    if (ISUserExits) {
-        return responder(res, 409, "Email already exists");
-    }
+        if (ISUserExits) {
+            return responder(res, 409, "Email already exists");
+        }
 
-    let hashPass = await bcrypt.hash(password, 10)
+        let hashPass = await bcrypt.hash(password, 10)
 
-    let CreateNewUser = await User.create({
-        name,
-        email,
-        password: hashPass
-    });
-
-
-    let saveUser = await CreateNewUser.save();
-
-    if (saveUser) {
-        return responder(res, 201, "User Created Successfully", {
-            name: newUser.name,
-            email: newUser.email
+        let CreateNewUser = await User.create({
+            name,
+            email,
+            password: hashPass
         });
+
+
+        let saveUser = await CreateNewUser.save();
+
+        if (saveUser) {
+            return responder(res, 201, "User Created Successfully", {
+                name,
+                email
+            });
+        }
+
+    } catch (error) {
+        return responder(res, 400, error, null)
     }
 
-    const LoginApi = async (req, res) => {
-        try {
-            let { email, password } = req.body;
 
-            if ( !email || !password) {
-                return responder(res, 406, "Email, Password Must be required", []);
-            }
+}
 
-            let FindUSER = await User.findOne({ email });
+const LoginApi = async (req, res) => {
+    try {
+        let { email, password } = req.body;
 
-            if (!FindUSER) {
-                return responder(res, 401, "User not Exits", null);
-            }
+        if (!email || !password) {
+            return responder(res, 400, "Email, Password are required", null);
+        }
 
-            isPassMatch = await bcrypt.compare(password,FindUSER.password);
+        let FindUSER = await User.findOne({ email });
 
-            if(!isPassMatch){
-                return responder(res,404,"INValid Credential",null);
-            }
+        if (!FindUSER) {
+            return responder(res, 404, "User does not exits", null);
+        }
 
-  let token = jwt.sign({
+        let isPassMatch = await bcrypt.compare(password, FindUSER.password);
+
+        if (!isPassMatch) {
+            return responder(res, 401, "Invalid Credential", null);
+        }
+
+        let token = jwt.sign({
             id: FindUSER._id,
             name: FindUSER.name,
             email: FindUSER.email
         }, process.env.JWT_SECRET);
 
+
         req.session.token = token;  // store token in Our session...
 
         return responder(res, 200, "login successfully", null);
 
-
-
-        } catch (error) {
-            return responder(res, 400, error, null);
-        }
-
+    } catch (error) {
+        return responder(res, 401, error, null);
     }
 
 }
+
+
 
 export { SignUp, LoginApi };
--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
-import session from "express-session"; 
+import session from "express-session";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -13,12 +13,12 @@ const PORT = 3000 || process.env.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
-    secret  : process.env.SESSION_SECRETE, //Used to sign the session ID cookie.
-    resave : false,   //session won’t be re-saved if nothing changed.// This reduces unnecessary writes to the session store.
-    saveUninitialized : false, //sessions won’t be stored until something is added (like user login info)
-    cookie:{
-        httpOnly : true, //Cookie can’t be accessed via JavaScript(document.cookie) protect
-        maxAge : 1000 * 60 *60 * 24 * 7  //7 days lifespan
+    secret: process.env.SESSION_SECRETE, //Used to sign the session ID cookie.
+    resave: false,   //session won’t be re-saved if nothing changed.// This reduces unnecessary writes to the session store.
+    saveUninitialized: false, //sessions won’t be stored until something is added (like user login info)
+    cookie: {
+        httpOnly: true, //Cookie can’t be accessed via JavaScript(document.cookie) protect
+        maxAge: 1000 * 60 * 60 * 24 * 7  //7 days lifespan
     }
 }))
 
@@ -28,10 +28,10 @@ import connectDB from "./Config/ConnectDB.js";
 
 
 //------Controllers----------
-import { SignUp , LoginApi } from "./Controllers/auth.control.js";
+import { SignUp, LoginApi } from "./Controllers/auth.control.js";
 
 app.post("/api/signup", SignUp);
-app.post("api/login",LoginApi);
+app.post("/api/login", LoginApi);
 
 app.get("./health", (req, res) => {
     return req.status(200).json({ data: Date.now(), message: "Server Running Healthy" })
