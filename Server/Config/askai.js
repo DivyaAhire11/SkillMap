26f63dce11739256b052e8aa40cdce298a5d75b3
DivyaAhire11 <divyaahire121@gmail.com>
2025-11-17 23:23:22 +0530

    Using ai ask question to AI

--- Server/Config/askai.js
+++ Server/Config/askai.js
@@ -1,26 +1,35 @@
 import OpenAI from "openai";
-import responder from "../Utils/responder";
 
-const askai = async(question = "you are rodmap ai and your name is learnmap.ai")=>{
+/*
+It takes a question parameter.
+
+If no question is provided, it uses the default string:
+"you are rodmap ai and your name is learnmap.ai".
+
+*/
+const askai = async (question = "you are rodmap ai and your name is learnmap.ai") => {
     try {
-        
+
         const openai = new OpenAI({
-            baseURL : "https://openrouter.ai/api/v1",
-            apiKey : process.env.LEARNMAP_AI_API 
+            baseURL: "https://openrouter.ai/api/v1",
+            apiKey: process.env.LEARNMAP_AI_API
         })
 
-        const responce = await openai.chat.completions.create({
-            model : "mistralai/ministral-3d",
-            messages:[
+        const response = await openai.chat.completions.create({
+            model: "mistralai/ministral-3d",//Specifies which model to use.
+            // we're using: mistralai/ministral-3d through OpenRouter.
+            messages: [
                 {
-                    "roe" : "user",
-                    "constent" : question
+                    "role": "user",   // tells the model who is speaking
+                    "content": question  //sends the user’s input message to the model
                 }
             ]
         })
-        return (responce.choices[0].message.content);
+        return (response.choices[0].message.content);  //Reads the model’s text output
 
     } catch (error) {
         return (error.message);
     }
 }
+
+export default askai;
